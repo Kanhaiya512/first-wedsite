5135d5e259b83dd558b8102054c54bfce8c2acd3
Kanhaiya <kanhaiyalalchauhan924@gmail.com>
2025-12-31 12:16:52 +0530

    Add some line comment

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
 name = "Kanhaiya"
 print(f"My name is {name}")
 
+"""
+After my class, I went to the 
+cafeteria. I waw alone because my 
+friend didn't come today. At first,
+if felt a little weird sitting there
+by myself, but I didn't mind.
+
+"""
